5b935a3e34aba22dd413b756b22423f005558b39
Ajazzan <71468728+Ajazzan@users.noreply.github.com>
2022-05-31 16:22:29 -0400

    Add files via upload

---
+++ functions/newAccount.js
@@ -0,0 +1,78 @@
+const db = require("megadb");
+const sha256 = require("sha256");
+
+//DB
+
+let money = new db.crearDB("money");
+let password = new db.crearDB("password");
+let cvv = new db.crearDB("cvv");
+let cardId = new db.crearDB("cardId");
+let code = new db.crearDB("code");
+
+//Functions
+
+function rng(length) {
+	var result           = '';
+	var characters       = '0123456789';
+	var charactersLength = characters.length;
+	for ( var i = 0; i < length; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * 
+			charactersLength));
+	}
+	return result;
+}
+
+async function genCardId(type) {
+		let finalNumber;
+		if(type === 1){
+		finalNumber = 9
+	} else if(type === 0){
+		finalNumber = 0
+	}
+	return `${rng(4)}-${rng(4)}-${rng(4)}-${rng(3)}${finalNumber}`
+}
+
+async function crearCuenta(pin,clientPassword,type) {
+
+	let gCardId = await genCardId(type)
+
+	if(cardId.tiene(gCardId)){
+		gCardId = genCardId()
+		if(cardId.tiene(gCardId)){
+			gCardId = genCardId()
+			if(cardId.tiene(gCardId)){
+				gCardId = genCardId()
+				if(cardId.tiene(gCardId)){
+					gCardId = genCardId()
+				}
+			}
+		}
+	}
+
+	if(!pin) {
+		return 'Especifique el pin.';
+	}
+
+	if(!type) {
+		return 'Especifique el tipo de tarjeta.'
+	}
+	if(!password) {
+		return 'Especifique la clave.'
+	}
+
+	let gcvv = rng(3)
+
+	let pinSha256 = await sha256(`${pin}`);
+	let passwordSha256 = await sha256(`${clientPassword}`);
+	let cvvSha256 = await sha256(`${gcvv}`);
+
+	await code.establecer(gCardId, `${pinSha256}`);
+	await cardId.establecer(gCardId, "true");
+	await money.establecer(gCardId, "0");
+	await password.establecer(gCardId, `${passwordSha256}`);
+	await cvv.establecer(gCardId, `${cvvSha256}`);
+
+	console.log(`Pin: ${pin}, CVV: ${gcvv}`)
+}
+
+crearCuenta(1234,12345678,1)
---
+++ functions/transaction.js
@@ -0,0 +1,85 @@
+const db = require("megadb");
+const sha256 = require("sha256");
+
+//DB
+
+let money = new db.crearDB("money");
+let password = new db.crearDB("password");
+let cvv = new db.crearDB("cvv");
+let cardId = new db.crearDB("cardId");
+let code = new db.crearDB("code");
+
+//Exclusive DB
+
+let references = new db.crearDB("references")
+let referencescount = new db.crearDB("referencescount")
+
+//Functions
+
+async function rng(length) {
+	var result           = '';
+	var characters       = '0123456789';
+	var charactersLength = characters.length;
+	for ( var i = 0; i < length; i++ ) {
+		result += characters.charAt(Math.floor(Math.random() * 
+			charactersLength));
+	}
+	return result;
+}
+
+async function trasaction(sCvv, sCardId, sAmount, sPin, rCardId) {
+	let sCvvSha256 = await sha256(`${sCvv}`);
+	let sPinSha256 = await sha256(`${sPin}`);
+
+	if(!cardId.tiene(sCardId)){
+		return "Wrong Sender Card ID"
+	}
+
+	if(!cardId.tiene(rCardId)){
+		return "Wrong Reciver Card ID"
+	}
+
+	let sCvvOnDb = await cvv.obtener(sCardId);
+
+	if(!sCvvOnDb === sCvvSha256){
+		return "Wrong Pin."
+	}
+
+	let sPinOnDB = code.obtener(sCardId)
+
+	if(!sPinOnDB === sPinSha256){
+		return "Wrong Pin."
+	}
+
+	let smoney = await money.obtener(sCardId)
+
+	if(sAmount > smoney) {
+		return 'You dont have the necesary money for this payment.'
+	}
+
+	let refnum = await referencescount.obtener("count")
+	let treference = await refnum + 1;
+	await referencescount.establecer("count", treference)
+
+	console.log(treference)
+
+	await money.restar(sCardId, sAmount)
+	await money.sumar(rCardId, sAmount)
+	await references.establecer(`${treference}.sender`, sCardId)
+	await references.establecer(`${treference}.reciver`, rCardId)
+	await references.establecer(`${treference}.amont`, sAmount)
+	await references.establecer(`${treference}.reference`, treference)
+
+	await money.sumar(sCardId, 10)
+
+	return {
+		"sender": sCardId,
+		"reciver": rCardId,
+		"amount": sAmount,
+		"reference": treference
+	}
+}
+
+setInterval(async () => {
+   await trasaction(129, "1945-5838-3382-4109", 1, 1234, "6133-1794-2545-3429")
+}, 1)
